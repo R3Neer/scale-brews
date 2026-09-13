0db6bb7d30176836b7d124695efda55ed3ff28f4
Samuel <64842010+R3Neer@users.noreply.github.com>
2026-09-13 16:02:33 +0200

    fix: keep item-steered bees on vanilla saddle UX

--- src/main/java/io/github/r3neer/scalebrews/mixin/TinyMountInventoryScreenMixin.java
+++ src/main/java/io/github/r3neer/scalebrews/mixin/TinyMountInventoryScreenMixin.java
@@ -5,14 +5,13 @@ import io.github.r3neer.scalebrews.mount.TinyMounts;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.HasCustomInventoryScreen;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.animal.bee.Bee;
 import net.minecraft.world.entity.animal.chicken.Chicken;
 import net.minecraft.world.entity.animal.wolf.Wolf;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 
-/** E while riding opens the same equipment surface players expect from vanilla mounts. */
-@Mixin({Chicken.class, Bee.class, Wolf.class})
+/** E opens mount equipment only for horse-like direct-control tiny mounts. */
+@Mixin({Chicken.class, Wolf.class})
 public abstract class TinyMountInventoryScreenMixin implements HasCustomInventoryScreen {
     @Override
     public void openCustomInventoryScreen(Player player) {
